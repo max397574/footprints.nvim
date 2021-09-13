df79485d8911a21e7d47cf86d6343eb22cdf2aff
max397574 <max.heinzer397@gmx.ch>
2021-09-13 20:33:41 +0200

    refactor(plugins): ♻️ removed unused functions

--- lua/footprints_nvim.lua
+++ lua/footprints_nvim.lua
@@ -3,6 +3,17 @@ local M = {}
 
 local settings = {}
 
+settings.highlight_color_1 = "#000000"
+settings.highlight_color_2 = "#2E2E2E"
+settings.highlight_color_3 = "#616161"
+settings.highlight_color_4 = "#969696"
+settings.highlight_color_5 = "#A1A1A1"
+settings.change_step_size = 1
+
+function M.setup(update)
+  settings = setmetatable(update, { __index = settings })
+end
+
 function M.add_highlights(changes)
   local buffer_nr = api.nvim_get_current_buf()
   for s in changes:gmatch("[^\n]+") do
@@ -25,26 +36,6 @@ function M.add_highlights(changes)
     end
   end
 end
-
-
-settings.highlight_color_1 = "#000000"
-settings.highlight_color_2 = "#2E2E2E"
-settings.highlight_color_3 = "#616161"
-settings.highlight_color_4 = "#969696"
-settings.highlight_color_5 = "#A1A1A1"
-settings.change_step_size = 1
-
-function M.highlight_line()
-  local buffer_nr = api.nvim_get_current_buf()
-  local line_nr = api.nvim_win_get_cursor(0)[1]
-  api.nvim_buf_add_highlight(buffer_nr, -1, "FootprintsLine", line_nr-1, 0, -1)
-end
-
-
-function M.setup(update)
-  settings = setmetatable(update, { __index = settings })
-end
-
 
 local function create_autocmds()
   vim.cmd[[autocmd TextChangedI * lua require("footprints_nvim").add_highlights(vim.fn.execute("changes"))]]
